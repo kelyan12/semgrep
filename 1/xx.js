05510d9fd574c63615ec5306cc51bd831798ddb3
Kelyan <125512238+kelyan12@users.noreply.github.com>
2026-03-17 11:41:13 +0100

    Update xx.js

--- 1/xx.js
+++ 1/xx.js
@@ -5,13 +5,19 @@ const router = express.Router()
 
 router.post('/upload-products', (req, res) => {
     const XMLfile = req.files.products.data;
-	const products = libxmljs.parseXmlString(XMLfile, {noent:true,noblanks:true})
+    
+    // CORRECTION : noent passe à false, et on ajoute nonet à true par sécurité
+    const products = libxmljs.parseXmlString(XMLfile, {
+        noent: false, 
+        nonet: true,
+        noblanks: true
+    })
 
-	products.root().childNodes().forEach(product => {
-		let newProduct = new db.Product()
-		newProduct.name = product.childNodes()[0].text()
-		newProduct.description = product.childNodes()[3].text()
-		newProduct.save()
+    products.root().childNodes().forEach(product => {
+        let newProduct = new db.Product()
+        newProduct.name = product.childNodes()[0].text()
+        newProduct.description = product.childNodes()[3].text()
+        newProduct.save()
     });
     
     res.send('Thanks')
